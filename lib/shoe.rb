2c669bf1d5c3c6f7a9874241707a0f6e0d737fa6
PJ Frias <peejfrias@gmail.com>
2018-01-06 20:20:02 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,9 @@
 class Shoe
   attr_accessor :condition, :brand, :color, :size, :material
 
-  def initialize(condition)
-    @condition = condition
+  def initialize(brand)
+    @condition = new
+    @brand = brand
   end
 
   def cobble
