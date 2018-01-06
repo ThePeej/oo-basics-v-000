c0108139e7bb2503f3cd562af1497b1bff3f9241
PJ Frias <peejfrias@gmail.com>
2018-01-06 17:02:25 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  attr_accessor :title, :author_name, :page_count
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+  
+  def turn_page
+    @page ++
+  end
 
